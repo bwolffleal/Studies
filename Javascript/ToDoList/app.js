7d0057fbba48ac1accdc78bd65479755ae40a16f
bwolffleal <bernardowleal@gmail.com>
2023-12-06 02:59:52 -0300

    Finished JavaScript course

--- Javascript/ToDoList/app.js
+++ Javascript/ToDoList/app.js
@@ -2,8 +2,23 @@
 const todoList = document.querySelector('#todo-list');
 const form = document.querySelector('#add-todo-form');
 const updateBtn = document.querySelector('#update');
+const logoutItems = document.querySelectorAll('.logged-out');
+const loginItems = document.querySelectorAll('.logged-in');
+let currentUser = null;
 let newTitle = '';
 let updateId = null;
+
+function setupUI(user) {
+    if (user) {
+        loginItems.forEach(item => item.style.display = 'block');
+        logoutItems.forEach(item => item.style.display = 'none');
+
+    } else {
+        loginItems.forEach(item => item.style.display = 'none');
+        logoutItems.forEach(item => item.style.display = 'block');
+    }
+}
+
 function renderList(doc) {
     let li = document.createElement('li');
     li.className = "collection-item";
@@ -12,7 +27,7 @@ function renderList(doc) {
     let title = document.createElement('span');
     title.textContent = doc.data().title;
     let anchor = document.createElement('a');
-    anchor.href = "#modal1";
+    anchor.href = "#modal-edit";
     anchor.className = "modal-trigger secondary-content";
     let editBtn = document.createElement('i');
     editBtn.className = "material-icons";
@@ -27,7 +42,7 @@ function renderList(doc) {
     li.appendChild(div);
     deleteBtn.addEventListener('click', e => {
         let id = e.target.parentElement.parentElement.getAttribute('data-id');
-        db.collection('todos').doc(id).delete();
+        db.collection('alltodos').doc(currentUser.uid).collection('todos').doc(id).delete();
     })
     editBtn.addEventListener('click', e => {
         updateId = e.target.parentElement.parentElement.parentElement.getAttribute('data-id');
@@ -40,31 +55,44 @@ function renderList(doc) {
 updateBtn.addEventListener('click', e => {
 
     newTitle = document.getElementsByName('newtitle')[0].value;
-    db.collection('todos').doc(updateId).update({
+    db.collection('alltodos').doc(currentUser.uid).collection('todos').doc(updateId).update({
         title: newTitle
     })
 })
 form.addEventListener('submit', e => {
     e.preventDefault();
-    db.collection('todos').add({
+    db.collection('alltodos').doc(currentUser.uid).collection('todos').add({
         title: form.title.value
+    }).catch(err => {
+        console.log(err.message)
     })
     form.title.value = '';
 })
+function getTodos() {
+    todoList.innerHTML = '';
+    currentUser = auth.currentUser;
 
-db.collection('todos').orderBy('title').onSnapshot(snapshot => {
-    let changes = snapshot.docChanges()
-    changes.forEach(change => {
-        if (change.type == 'added') {
-            renderList(change.doc);
+    document.querySelector('#user-email').innerHTML = (currentUser != null ? currentUser.email : '');
 
-        } else if (change.type == 'removed') {
-            let li = todoList.querySelector(`[data-id=${change.doc.id}]`);
-            todoList.removeChild(li);
-        } else if (change.type == 'modified') {
-            let li = todoList.querySelector(`[data-id=${change.doc.id}]`);
-            li.getElementsByTagName('span')[0].textContent = newTitle;
-            newTitle = '';
-        }
-    });
-})
+    console.log('currentUser', currentUser)
+    if (currentUser === null) {
+        todoList.innerHTML = '<h3 class="center-align">Please login to get todos</h3>';
+        return;
+    }
+    db.collection('alltodos').doc(currentUser.uid).collection('todos').orderBy('title').onSnapshot(snapshot => {
+        let changes = snapshot.docChanges()
+        changes.forEach(change => {
+            if (change.type == 'added') {
+                renderList(change.doc);
+
+            } else if (change.type == 'removed') {
+                let li = todoList.querySelector(`[data-id=${change.doc.id}]`);
+                todoList.removeChild(li);
+            } else if (change.type == 'modified') {
+                let li = todoList.querySelector(`[data-id=${change.doc.id}]`);
+                li.getElementsByTagName('span')[0].textContent = newTitle;
+                newTitle = '';
+            }
+        });
+    })
+}
